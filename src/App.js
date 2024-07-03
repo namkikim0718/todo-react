a4274e20b7d2e3831a082f088808d0e1ac9ed1a4
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-07-03 16:00:04 +0900

    feat: todoList 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+        setTodos([
+            ...todos,
+            { id: Date.now(), text, completed: false }
+        ]);
+    };
+
+    const toggleTodo = (id) => {
+        setTodos(todos.map(todo => 
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ));
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
+    return (
+        <div className="App">
+            <h1>Todo List</h1>
+            <TodoInput addTodo={addTodo} />
+            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        </div>
+    );
 }
 
 export default App;
